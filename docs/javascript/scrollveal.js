20344b226c0f71300f8582ec5881a20fbade2f0a
elisatodd <elitodd@ucm.es>
2024-10-29 15:59:43 +0100

    Update

--- docs/javascript/scrollveal.js
+++ docs/javascript/scrollveal.js
@@ -2,48 +2,48 @@ const defaultProps = {
   easing: "cubic-bezier(0.5, 0, 0, 1)",
   distance: "30px",
   origin: "bottom",
-  duration: 1000,
+  duration: 300,
   desktop: true,
   mobile: true,
 };
 // Hero Section
 ScrollReveal().reveal(".hero-title", {
   ...defaultProps,
-  delay: 500,
+  delay: 200,
   origin: window.innerWidth > 768 ? "left" : "bottom",
 });
 ScrollReveal().reveal(".name", {
   ...defaultProps,
   distance: "0px",
-  delay: 700,
+  delay: 200,
   scale: 0,
 });
 ScrollReveal().reveal(".hero-cta", {
   ...defaultProps,
-  delay: 1200,
+  delay: 200,
   origin: window.innerWidth > 768 ? "left" : "bottom",
 });
 ScrollReveal().reveal(".scroll-down-link", {
   ...defaultProps,
-  delay: 2000,
+  delay: 200,
   origin: "top",
 });
 
 /* About Section */
 ScrollReveal().reveal(".section-title", {
   ...defaultProps,
-  delay: 400,
+  delay: 200,
   distance: "0px",
 });
 ScrollReveal().reveal(".about-wrapper__image", {
   ...defaultProps,
-  delay: 600,
+  delay: 200,
   scale: 0.5,
 });
 
 ScrollReveal().reveal(".about-wrapper__info", {
   ...defaultProps,
-  delay: 700,
+  delay: 200,
   distance: "100px",
   origin: window.innerWidth > 768 ? "left" : "top",
 });
@@ -51,13 +51,13 @@ ScrollReveal().reveal(".about-wrapper__info", {
 /* Projects Section */
 ScrollReveal().reveal(".project-wrapper__text", {
   ...defaultProps,
-  delay: 600,
+  delay: 200,
   origin: window.innerWidth > 768 ? "left" : "bottom",
 });
 
 ScrollReveal().reveal(".project-wrapper__image", {
   ...defaultProps,
-  delay: 600,
+  delay: 200,
   scale: 0.9,
   origin: window.innerWidth > 768 ? "right" : "bottom",
 });
@@ -65,11 +65,11 @@ ScrollReveal().reveal(".project-wrapper__image", {
 /* Contact Section */
 ScrollReveal().reveal(".contact-wrapper", {
   ...defaultProps,
-  delay: 600,
+  delay: 200,
 });
 
 ScrollReveal().reveal(".fa", {
   ...defaultProps,
-  delay: 500,
+  delay: 200,
   interval: 100,
 });
